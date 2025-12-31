e6dbdb12745760b11811c73880dd30af801f12d2
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2025-12-31 11:12:02 +0800

    12_31 angid

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,7 @@
 import style from "./page.module.css";
 export default function Home() {
-  //-page
   return (
-    <div>
+    <div className="h-screen  flex justify-center items-center">
       {/* <div className={style.box}>Hello</div>; */}
 
       <Profile />
@@ -13,41 +12,80 @@ export default function Home() {
 //   return <p>hello</p>;
 // };
 
+// const Profile = () => {
+//   return (
+//     <div className={style.bod}>
+//       <div className={style.container}>
+//         <img
+//           className={style.img}
+//           src="https://newprofilepic.photo-cdn.net//assets/images/article/profile.jpg?90af0c8"
+//           alt="profile"
+//         />
+
+//         <div className={style.userInfo}>
+//           <div className={style.ner}>
+//             <h1 className={style.nerP}>Sophie Bennett</h1>
+//             <img
+//               className={style.nerIcon}
+//               src="/verificationContainer.svg"
+//               alt="icon"
+//             />
+//           </div>
+//           <p className={style.userInfo__desc}>
+//             Product Designer who focuses on simplicity & usability.
+//           </p>
+//         </div>
+//         <div className={style.too}>
+//           <div className={style.userStat}>
+//             <img src="/followersContainer.svg" alt="followers" />
+//             <p className={style.dagagch}>312</p>
+//           </div>
+//           <div className={style.userStat}>
+//             <img src="/projectsContainer.svg" alt="projectsContainer" />
+//             <p className={style.dagagch}>48</p>
+//           </div>
+//           <div className={style.btn}>
+//             <button className={style.btnSelf}>Follow +</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
 const Profile = () => {
   return (
-    <div className={style.bod}>
-      <div className={style.container}>
+    <div className="bg-gray-100 shadow-md py-5 px-4 w-fit h-fit rounded-md">
+      <div>
         <img
-          className={style.img}
-          src="https://newprofilepic.photo-cdn.net//assets/images/article/profile.jpg?90af0c8"
+          src="https://newprofilepic.photo-cdn.net/assets/images/article/profile.jpg?90af0c8"
           alt="profile"
+          className="w-[280] h-[479] rounded-lg "
         />
-
-        <div className={style.userInfo}>
-          <div className={style.ner}>
-            <h1 className={style.nerP}>Sophie Bennett</h1>
-            <img
-              className={style.nerIcon}
-              src="/verificationContainer.svg"
-              alt="icon"
-            />
-          </div>
-          <p className={style.userInfo__desc}>
-            Product Designer who focuses on simplicity & usability.
-          </p>
+      </div>
+      <div>
+        <div className="flex gap-6">
+          <p className="text-xl">Sophie Bennett</p>
+          <img src="/verificationContainer.svg" alt="icon" />
         </div>
-        <div className={style.too}>
-          <div className={style.userStat}>
-            <img src="/followersContainer.svg" alt="followers" />
-            <p className={style.dagagch}>312</p>
-          </div>
-          <div className={style.userStat}>
-            <img src="/projectsContainer.svg" alt="projectsContainer" />
-            <p className={style.dagagch}>48</p>
-          </div>
-          <div className={style.btn}>
-            <button className={style.btnSelf}>Follow +</button>
-          </div>
+        <p className="text-base text-gray-500 w-[300]">
+          Product Designer who focuses on simplicity & usability.
+        </p>
+      </div>
+
+      <div className="flex gap-6">
+        <div className="flex gap-1 items-center">
+          <img src="/followersContainer.svg" alt="followers" />
+          <p className={style.dagagch}>312</p>
+        </div>
+        <div className="flex gap-1 items-center">
+          <img src="/projectsContainer.svg" alt="projectsContainer" />
+          <p className={style.dagagch}>48</p>
+        </div>
+        <div>
+          <button className="bg-[rgba(239,239,239,1)] text-lg p-2 rounded-2xl ">
+            Follow +
+          </button>
         </div>
       </div>
     </div>
