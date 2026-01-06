b22df2bed3e7f7d6960c193790fe15a0d506f2e3
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2026-01-06 13:54:26 +0800

    01.05 angid

--- src/app/ourTours/page.jsx
+++ src/app/ourTours/page.jsx
@@ -2,7 +2,11 @@
 import { useState } from "react";
 export default function Home() {
   return (
-    <div className="flex justify-center bg-gray-100 h-screen">
+    <div className=" bg-gray-100 h-screens flex flex-col gap-6 justify-center items-center">
+      <h1 className="text-4xl pb-4 text-center border-b-4 w-100 border-b-green-500">
+        Our Tours
+      </h1>
+
       <TourList />
     </div>
   );
@@ -17,42 +21,28 @@ const tour = [
   },
   {
     id: 2,
-    image: "/paris.png",
+    image: "/ireland.png",
     price: "",
     title: "Best Of Paris In 7 Days Tour",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum sequi illo sed nam impedit blanditiis maiores, repudiandae alias deleniti. A eius incidunt veritatis repellendus aut? Dolor exercitationem a tempora!",
   },
   {
     id: 3,
-    image: "/paris.png",
+    image: "/vienna.png",
     price: "",
     title: "Best Of Paris In 7 Days Tour",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum sequi illo sed nam impedit blanditiis maiores, repudiandae alias deleniti. A eius incidunt veritatis repellendus aut? Dolor exercitationem a tempora!",
   },
   {
     id: 4,
-    image: "/paris.png",
+    image: "/rome.png",
     price: "",
     title: "Best Of Paris In 7 Days Tour",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum sequi illo sed nam impedit blanditiis maiores, repudiandae alias deleniti. A eius incidunt veritatis repellendus aut? Dolor exercitationem a tempora!",
   },
   {
     id: 5,
-    image: "/paris.png",
-    price: "",
-    title: "Best Of Paris In 7 Days Tour",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum sequi illo sed nam impedit blanditiis maiores, repudiandae alias deleniti. A eius incidunt veritatis repellendus aut? Dolor exercitationem a tempora!",
-  },
-  {
-    id: 6,
-    image: "/paris.png",
-    price: "",
-    title: "Best Of Paris In 7 Days Tour",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum sequi illo sed nam impedit blanditiis maiores, repudiandae alias deleniti. A eius incidunt veritatis repellendus aut? Dolor exercitationem a tempora!",
-  },
-  {
-    id: 7,
-    image: "/paris.png",
+    image: "/poland.png",
     price: "",
     title: "Best Of Paris In 7 Days Tour",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum sequi illo sed nam impedit blanditiis maiores, repudiandae alias deleniti. A eius incidunt veritatis repellendus aut? Dolor exercitationem a tempora!",
@@ -64,21 +54,23 @@ const TourList = () => {
   const clearList = () => {
     setTourList([]);
   };
-  //   console.log(clearList);
   return (
-    <div>
-      <h1 className="text-4xl p-2">Our Tours</h1>
-      <p className="text-green-300 p-3 font-bold">----------------------</p>
-      {tourList.map(({ id, image, price, title, text }) => (
-        <TourListItem
-          key={id}
-          image={image}
-          price={price}
-          title={title}
-          text={text}
-          clearList={clearList}
-        />
-      ))}
+    <div className=" flex justify-center">
+      <div></div>
+      <div>
+        <div className="grid grid-cols-3 gap-3">
+          {tourList.map(({ id, image, price, title, text }) => (
+            <TourListItem
+              key={id}
+              image={image}
+              price={price}
+              title={title}
+              text={text}
+              clearList={clearList}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
@@ -86,15 +78,15 @@ const TourList = () => {
 const TourListItem = ({ image, price, title, text, clearList }) => {
   return (
     <div>
-      <div className="w-150 h-200 bg-white flex flex-col rounded-2xl ">
+      <div className="w-90 h-160 bg-white flex flex-col rounded-2xl ">
         <div>
-          <img className="w-150 h-125 rounded-2xl" src={image} /> <p>{price}</p>
+          <img className="w-90 h-80 rounded-2xl" src={image} /> <p>{price}</p>
         </div>
         <div>
           <h3 className="pt-6 p-2.5 flex justify-center text-3xl font-bold">
             {title}
           </h3>
-          <p className=" p-6 flex justify-center">{text}</p>
+          <p className="ml-2 p-2 flex justify-center">{text}</p>
         </div>
         <div className="flex justify-center">
           <button
