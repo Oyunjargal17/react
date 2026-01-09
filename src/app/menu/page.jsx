fae03e5c082371a84b612a881c0d2dfa1f882b9c
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2026-01-09 09:49:03 +0800

    2026.01.09 angid

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const menus = [
   {
     id: 1,
+    type: "breakfast",
     image: "https://react-vite-projects-5-menu.netlify.app/images/item-1.jpeg",
     title: "buttermilk pancakes",
     price: "$15.99",
@@ -10,6 +11,7 @@ const menus = [
   },
   {
     id: 2,
+    type: "lunch",
     image: "https://react-vite-projects-5-menu.netlify.app/images/item-2.jpeg",
     title: "diner double",
     price: "$13.99",
@@ -17,10 +19,59 @@ const menus = [
   },
   {
     id: 3,
+    type: "shakes",
     image: "https://react-vite-projects-5-menu.netlify.app/images/item-3.jpeg",
     title: "godzilla milkshake",
     price: "$6.99",
     text: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
+  },
+  {
+    id: 4,
+    type: "breakfast",
+    image: "https://react-vite-projects-5-menu.netlify.app/images/item-4.jpeg",
+    title: "country delight",
+    price: "$20.99",
+    text: "I'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed I'm",
+  },
+  {
+    id: 5,
+    type: "lunch",
+    image: "https://react-vite-projects-5-menu.netlify.app/images/item-5.jpeg",
+    title: "egg attack",
+    price: "$22.99",
+    text: "franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up",
+  },
+  {
+    id: 6,
+    type: "shakes",
+    image: "https://react-vite-projects-5-menu.netlify.app/images/item-6.jpeg",
+    title: "oreo dream",
+    price: "$18.99",
+    text: "Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday",
+  },
+  {
+    id: 7,
+    type: "breakfast",
+    image: "https://react-vite-projects-5-menu.netlify.app/images/item-7.jpeg",
+    title: "bacon overflow",
+    price: "$8.99",
+    text: "carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
+  },
+  {
+    id: 8,
+    type: "lunch",
+    image: "https://react-vite-projects-5-menu.netlify.app/images/item-8.jpeg",
+    title: "american classic",
+    price: "$12.99",
+    text: "on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut",
+  },
+  {
+    id: 9,
+    type: "shakes",
+    image: "https://react-vite-projects-5-menu.netlify.app/images/item-9.jpeg",
+    title: "quarantine buddy",
+    price: "$16.99",
+    text: "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
   },
 ];
 export default function Home() {
